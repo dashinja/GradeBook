8fe13bea5fad7fb5de41cbef4e6d3b38e9f83e98
DaShinja <byron@dashinja.com>
2019-08-10 21:54:49 -0400

    Verifies StatusQuo Unit Test works

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -6,7 +6,7 @@ namespace GradeBook.Tests
   public class BookTests
   {
     [Fact]
-    public void Test1()
+    public void StatusQuo()
     {
       // arrange section: objects and values
       var book = new Book("", "", "");
