376eb55537857b4dd016a216b0354e901347cdc8
Byron Miller <byron.miller@lowes.com>
2019-08-13 09:07:55 -0400

    never have I ever git, foreal

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -6,7 +6,7 @@ namespace GradeBook.Tests
   public class BookTests
   {
     [Fact]
-    public void StatusQuo()
+    public void BookCalculatesAnAverageGrade()
     {
       // arrange section: objects and values
       var book = new Book("", "", "");
@@ -21,6 +21,36 @@ namespace GradeBook.Tests
       Assert.Equal(85.6, result.Average, 1);
       Assert.Equal(90.5, result.High, 1);
       Assert.Equal(77.3, result.Low, 1);
+    }
+
+    [Fact]
+    public void parameterMatch()
+    {
+      //arrange
+      var book = new Book("favorite", "Me", "maths");
+
+      //act
+      var actualTeacher = book.TeacherName;
+      var expectedTeacher = "favorite";
+      var actualStudent = book.StudentName;
+      var expectedStudent = "Me";
+
+      //assert
+      Assert.Equal(expectedTeacher, actualTeacher);
+      Assert.Equal(expectedStudent, actualStudent);
+    }
+
+    [Fact]
+    public void printResults()
+    {
+      //arrange
+      var book = new Book("Judy", "Megan", "Language Arts");
+
+      //act
+      var result = book.PrintStats(book);
+
+      //assert
+      Assert.Equal(result, "Printed All");
     }
   }
 }
