256657f2c7daed468d3c9bf992bb5e162def4b61
DaShinja <byron@dashinja.com>
2019-08-15 23:01:35 -0400

    removed unnecessary commands in program.cs

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -4,41 +4,37 @@ using System.Collections.Generic;
 namespace GradeBook
 {
 
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      Console.WriteLine("What is the teacher's name for this grade book?");
+      var teacherName = Console.ReadLine();
+      Console.WriteLine("What is the student's name for whom grades will be recorded:");
+      var studentName = Console.ReadLine();
+      Console.WriteLine("For which academic subject are the grades to be entered?");
+      var subjectMatter = Console.ReadLine();
+
+      var book = new Book(teacherName, studentName, subjectMatter);
+
+
+      while (true)
+      {
+        Console.WriteLine("Enter student grade - or 'q' to exit.");
+        var grade = Console.ReadLine();
+
+        if (grade == "q")
         {
-            Console.WriteLine("What is the teacher's name for this grade book?");
-            var teacherName = Console.ReadLine();
-            Console.WriteLine("What is the student's name for whom grades will be recorded:");
-            var studentName = Console.ReadLine();
-            Console.WriteLine("For which academic subject are the grades to be entered?");
-            var subjectMatter = Console.ReadLine();
-
-            var book = new Book(teacherName, studentName, subjectMatter);
-
-
-            while (true)
-            {
-                Console.WriteLine("Enter student grade - or 'q' to exit.");
-                var grade = Console.ReadLine();
-
-                if (grade == "q")
-                {
-                    break;
-                }
-                
-                book.AddGrade(double.Parse(grade));
-            }
-
-            book.ListGrades();
-            book.PrintStats(book);
-
-            Book.Description();
-
-            book.ClearGrades();
-
-            book.ListGrades();
+          break;
         }
+
+        book.AddGrade(double.Parse(grade));
+      }
+
+      book.ListGrades();
+      book.PrintStats(book);
+
+      Book.Description();
     }
+  }
 }
